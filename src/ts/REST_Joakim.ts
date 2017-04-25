d235eb7bb347ef64d9ae9866ac2ca4c89cd0f876
joakbjerk <joakbjerk@gmail.com>
2017-04-25 17:45:04 +0200

    Rest_joakim update

--- src/ts/REST_Joakim.ts
+++ src/ts/REST_Joakim.ts
@@ -1,10 +1,60 @@
 $(document).ready(function () {
-  getSubsiteHandlingsplan();
+  ExecuteOrDelayUntilScriptLoaded(init, 'sp.js');
 });
 
-function getSubsiteHandlingsplan() {
-  let ctx = SP.ClientContext.get_current();
-  let web = ctx.get_site().get_rootWeb();
-  console.log(ctx);
-  console.log(web);
+
+function init() {
+  let subsitesUrlArray = [];
+  getSubsiteUrls();
+
+  function getSubsiteUrls() {
+    console.log('getSubsiteUrls');
+    $.ajax({
+      url: _spPageContextInfo.webAbsoluteUrl + '/_api/web/webs',
+      type: 'GET',
+      headers: { 'Accept': 'application/json;odata=verbose', 'Content-Type': 'application/json;odata=verbose' },
+      success: onGetSubsiteSuccess,
+      error: onGetSubsiteError
+    });
+  }
+
+  function onGetSubsiteSuccess(data) {
+    let subsites = data.d.results;
+    for (let index = 0; index < subsites.length; index++) {
+      let subsite = subsites[index];
+      console.log(subsite.Url);
+      subsitesUrlArray.push(subsite.Url);
+    }
+    console.log('Subsites push complete');
+    console.log(subsitesUrlArray.length);
+    getSubsitesHandlingsplanListItems();
+  }
+
+  function onGetSubsiteError(data, errorMessage, errorCode) {
+    console.log(errorMessage);
+  }
+
+  function getSubsitesHandlingsplanListItems() {
+    console.log('getSubsitesHandlingsplanListItems');
+    console.log('Url Array Length check');
+    console.log(subsitesUrlArray.length);
+    for (let index = 0; index < subsitesUrlArray.length; index++) {
+      let subsiteUrl = subsitesUrlArray[index];
+      $.ajax({
+        url: subsiteUrl + '/_api/web/lists',
+        type: 'GET',
+        headers: { 'Accept': 'application/json;odata=verbose', 'Content-Type': 'application/json;odata=verbose' },
+        success: onGetSubsiteListItemsSuccess,
+        error: onGetSubsiteListItemsError
+      });
+    }
+  };
+
+  function onGetSubsiteListItemsSuccess(data) {
+    console.log(data.d.results);
+  }
+
+  function onGetSubsiteListItemsError(data, errorMessage, errorCode) {
+    console.log(errorMessage);
+  }
 }
