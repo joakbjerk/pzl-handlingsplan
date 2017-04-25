baf730cc1a9671a993477a598f3f97fb8473b694
aleksKoss <aleksander.kossacz@puzzlepart.com>
2017-04-25 15:29:20 +0200

    CustomAction for aleksander

---
+++ src/ts/REST_Aleksander.ts
@@ -0,0 +1,15 @@
+export function logListItems() {
+    console.log('tempLog');
+}
+
+export function getSubsites() {
+    let restUrl: string = `/_api/web/webs`;
+
+
+
+}
+
+export function getListItems() {
+    let restUrl: string;
+
+}
